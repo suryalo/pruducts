c9079633c87d0be130d303c60eb7cf7d65da4292
Surya <amy.renaissance@gmail.com>
2022-04-07 12:26:44 +0800

    add file exist

--- products.py
+++ products.py
@@ -1,16 +1,22 @@
-# 讀取檔案
-products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue    # 繼續跳到下一迴圈
-		name, price = line.strip().split(',') 
-		products.append([name, price])
+import os  # operating system 作業系統
 
-			#先用strip()把換行符號去掉
-			#再用‘,'當作切割的基準
-		
-print(products)
+
+# 讀取檔案
+products = []  # 把空清單移到 if 之外表示無論如何都要做
+
+if os.path.isfile('products.csv'):  # 檢查檔案在不在 
+
+	print('找到檔案了！')
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue    # 繼續跳到下一迴圈
+			name, price = line.strip().split(',') 
+			products.append([name, price])
+	print(products)
+
+else:
+	print('找不到檔案！')
 
 
 # 讓使用者輸入
